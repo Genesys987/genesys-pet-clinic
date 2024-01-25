4ad63d282b406b6c24cacabf85a00eedd570b858
Juhász Áron <aharonshepherd@gmail.com>
2024-01-25 20:11:22 +0100

    Initial Project Add

---
+++ src/main/java/com/example/genesyspetclinic/GenesysPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.genesyspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesysPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenesysPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/genesyspetclinic/GenesysPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genesyspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesysPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
