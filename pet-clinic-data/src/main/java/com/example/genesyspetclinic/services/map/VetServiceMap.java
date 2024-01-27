70bf5f3cd1ff9b44dc1ba36ed957439e5d55246a
Juhász Áron <aharonshepherd@gmail.com>
2024-01-27 03:05:10 +0100

    Implement Map based Services, commit to resolve #5

---
+++ pet-clinic-data/src/main/java/com/example/genesyspetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.example.genesyspetclinic.services.map;
+
+import com.example.genesyspetclinic.model.Vet;
+import com.example.genesyspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+}
